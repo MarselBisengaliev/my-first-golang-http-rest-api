46f1d8bde814e9756e2245a9f0a1170b75b12358
Marsel <marselbisengaliev1@gmail.com>
2023-06-07 17:50:40 +0500

    Небольшой рефакторинг
    занялся небольшим рефакторингом приложения. Научил сервер принимать зависимости из вне и упростил его. Также добавил дополнительную реализацию (мок) хранилища для тестов.

---
+++ internal/app/store/sqlstore/userrepository_test.go
@@ -0,0 +1,36 @@
+package sqlstore_test
+
+import (
+	store "http-rest-api/internal/app"
+	"http-rest-api/internal/app/model"
+	"http-rest-api/internal/app/store/sqlstore"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestUserRepository_Create(t *testing.T) {
+	db, teardown := sqlstore.TestDB(t, databaseUrl)
+	defer teardown("users")
+
+	s := sqlstore.New(db)
+	u := model.TestUser(t)
+	assert.NoError(t, s.User().Create(u))
+	assert.NotNil(t, u)
+}
+
+func TestUserRepository_FindByEmail(t *testing.T) {
+	db, teardown := sqlstore.TestDB(t, databaseUrl)
+	defer teardown("users")
+
+	s := sqlstore.New(db)
+	email := "user@example.org"
+	_, err := s.User().FindByEmail(email)
+	assert.EqualError(t, err, store.ErrRecordNotFound.Error())
+
+	u := model.TestUser(t)
+	s.User().Create(u)
+	u, err = s.User().FindByEmail(email)
+	assert.NoError(t, err)
+	assert.NotNil(t, u)
+}
--- internal/app/store/teststore/userrepository_test.go
+++ internal/app/store/teststore/userrepository_test.go
@@ -1,33 +1,26 @@
-package store_test
+package teststore_test
 
 import (
+	store "http-rest-api/internal/app"
 	"http-rest-api/internal/app/model"
-	"http-rest-api/internal/app/store"
+	"http-rest-api/internal/app/store/teststore"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func TestUserRepository_Create(t *testing.T) {
-	s, teardown := store.TestStore(t, databaseUrl)
-	defer teardown("users")
-
-	u, err := s.User().Create(&model.User{
-		Email: "user@example.org",
-		Password: "password",
-	})
-
-	assert.NoError(t, err)
+	s := teststore.New()
+	u := model.TestUser(t)
+	assert.NoError(t, s.User().Create(u))
 	assert.NotNil(t, u)
 }
 
 func TestUserRepository_FindByEmail(t *testing.T) {
-	s, teardown := store.TestStore(t, databaseUrl)
-	defer teardown("users")
-
+	s := teststore.New()
 	email := "user@example.org"
 	_, err := s.User().FindByEmail(email)
-	assert.Error(t, err)
+	assert.EqualError(t, err, store.ErrRecordNotFound.Error())
 
 	u := model.TestUser(t)
 	s.User().Create(u)
