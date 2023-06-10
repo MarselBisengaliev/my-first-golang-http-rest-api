9162a16300436a52a9b08e804c76ffa193d467e8
Marsel <marselbisengaliev1@gmail.com>
2023-06-10 23:35:30 +0500

    Middleware-компоненты
    добавил 4 middleware-компонента для API:
    - Проверка аутентификации пользователя
    - CORS-заголовки
    - Request ID
    - Логирование запросов

--- internal/app/apiserver/server_internal_test.go
+++ internal/app/apiserver/server_internal_test.go
@@ -3,15 +3,60 @@ package apiserver
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"http-rest-api/internal/app/model"
 	"http-rest-api/internal/app/store/teststore"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
+	"github.com/gorilla/securecookie"
 	"github.com/gorilla/sessions"
 	"github.com/stretchr/testify/assert"
 )
+
+func TestServer_AuthenticateUser(t *testing.T) {
+	store := teststore.New()
+	u := model.TestUser(t)
+	store.User().Create(u)
+
+	testCases := []struct {
+		name         string
+		cookieValue  map[interface{}]interface{}
+		expectedCode int
+	}{
+		{
+			name: "authenticated",
+			cookieValue: map[interface{}]interface{}{
+				"user_id": u.ID,
+			},
+			expectedCode: http.StatusOK,
+		},
+		{
+			name: "not authenticated",
+			cookieValue: nil,
+			expectedCode: http.StatusUnauthorized,
+		},
+	}
+
+	secretKey := []byte("secret")
+	s := newServer(store, sessions.NewCookieStore(secretKey))
+	sc := securecookie.New(secretKey, nil)
+	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	})
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			rec := httptest.NewRecorder()
+			req, _ := http.NewRequest(http.MethodGet, "/", nil)
+			cookieStr, _ := sc.Encode(sessionName, tc.cookieValue)
+			req.Header.Set("Cookie", fmt.Sprintf("%s=%s", sessionName, cookieStr))
+			s.authenticateUser(handler).ServeHTTP(rec, req)
+			assert.Equal(t, tc.expectedCode, rec.Code)
+		})
+	}
+}
 
 func TestServer_HandleUsersCreate(t *testing.T) {
 	s := newServer(teststore.New(), sessions.NewCookieStore([]byte("secret")))
@@ -69,20 +114,20 @@ func TestServer_HandleSessionsCreate(t *testing.T) {
 		{
 			name: "valid",
 			payload: map[string]string{
-				"email": u.Email,
+				"email":    u.Email,
 				"password": u.Password,
 			},
 			expectedCode: http.StatusOK,
 		},
 		{
-			name: "invalid payload",
-			payload: "invalid",
+			name:         "invalid payload",
+			payload:      "invalid",
 			expectedCode: http.StatusBadRequest,
 		},
 		{
 			name: "invalid email",
 			payload: map[string]string{
-				"email": "invalid",
+				"email":    "invalid",
 				"password": u.Password,
 			},
 			expectedCode: http.StatusUnauthorized,
@@ -90,7 +135,7 @@ func TestServer_HandleSessionsCreate(t *testing.T) {
 		{
 			name: "invalid password",
 			payload: map[string]string{
-				"email": u.Email,
+				"email":    u.Email,
 				"password": "invalid",
 			},
 			expectedCode: http.StatusUnauthorized,
